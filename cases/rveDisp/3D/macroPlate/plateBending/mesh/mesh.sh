6b530728c84f418fd38ab8a75d744bf43d4bc3cf
Adam Sciegaj <adam.sciegaj@gmail.com>
2021-02-26 11:24:20 +0100

    Added SM1, SM2, SM3 tests and moved mesh generation files

--- cases/rveDisp/3D/macroPlate/plateBending/mesh/mesh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-~/Software/generator/generator.exe generator.in.ctrl 
-qdelaunay Qt i < nodes.dat > delaunay.dat 
-qvoronoi p Fv < nodes.dat > voronoi.dat 
-~/Software/converter/converter.exe control.in.ctrl nodes.dat delaunay.dat voronoi.dat 
